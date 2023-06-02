55f2caa45d039437d8feb47e57905d9e0b5124c9
Tendsin Mende <tendsin@protonmail.com>
2023-06-02 12:56:31 +0200

    let FuncEnumerator also use the function vec instead

--- crates/patch-function/src/enumerate.rs
+++ crates/patch-function/src/enumerate.rs
@@ -1,8 +1,5 @@
 use smallvec::SmallVec;
-use spv_patcher::{
-    rspirv::{dr::Module, spirv::Op},
-    spirv_ext::SpirvExt,
-};
+use spv_patcher::{rspirv::dr::Module, spirv_ext::SpirvExt};
 
 use crate::function_finder::FuncSignature;
 
@@ -24,51 +21,24 @@ impl FuncEnumerator {
         //Walk the module, for each OpFunction, collect the return type-id,
         // as well as all argument type-ids.
         // Then check if we have a debug name.
-        let mut results = SmallVec::new();
-        let mut collection_decl = None;
-        for inst in spirv.all_inst_iter() {
-            match inst.class.opcode {
-                Op::Function => {
-                    //read function's result type and start collecting
-                    let return_type = inst.result_type.expect("Function had no return type!");
-
-                    let debug_name = spirv.get_name(inst.result_id.unwrap());
-
-                    //For sanity, check that we already exited the collection loop.
-                    assert!(
-                        collection_decl.is_none(),
-                        "Found OpFunction, but already collecting arguments."
-                    );
-                    collection_decl = Some(FuncDeclaration {
-                        debug_name,
-                        signature: FuncSignature {
-                            return_type,
-                            argument_types: SmallVec::new(),
-                        },
-                    });
+        spirv
+            .functions
+            .iter()
+            .map(|f| {
+                let debug_name = spirv.get_name(f.def.as_ref().unwrap().result_id.unwrap());
+                let signature = FuncSignature {
+                    return_type: f.def.as_ref().unwrap().result_type.unwrap(),
+                    argument_types: f
+                        .parameters
+                        .iter()
+                        .map(|p| p.result_type.unwrap())
+                        .collect(),
+                };
+                FuncDeclaration {
+                    debug_name,
+                    signature,
                 }
-                Op::FunctionParameter => {
-                    if let Some(collecting) = &mut collection_decl {
-                        let param_ty = inst
-                            .result_type
-                            .expect("Function parameter had no result type");
-                        collecting.signature.argument_types.push(param_ty);
-                    } else {
-                        log::warn!("Found function parameter, but not collecting function OP, is the module valid?");
-                    }
-                }
-                _ => {
-                    //Found some other instruction, finish collection.
-                    if let Some(coll) = collection_decl.take() {
-                        results.push(coll);
-                    }
-                }
-            }
-        }
-        //Post take as well if the last basic-block was a `import` declared function.
-        if let Some(coll) = collection_decl.take() {
-            results.push(coll);
-        }
-        results
+            })
+            .collect()
     }
 }
