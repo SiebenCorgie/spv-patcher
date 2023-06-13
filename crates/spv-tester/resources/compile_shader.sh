fc109284d5bcf4f2806bfa25a95bdf06784b4dce
Tendsin Mende <tendsin@protonmail.com>
2023-06-13 12:37:59 +0200

    fix build script to check for glslangValidator before using it.

--- crates/spv-tester/resources/compile_shader.sh
+++ crates/spv-tester/resources/compile_shader.sh
@@ -1,5 +1,8 @@
 #/bin/sh
+
+# Build rust shaders
 cargo build
+# then try to run GLSL->SPIR-V compiler
 glslangValidator -g -V compute_add.comp -o compute_add.spv
 glslangValidator -g -V nonuniform_patch.comp -o nonuniform_patch.spv
 glslangValidator -g -V forward_declare.comp -o forward_declare.spv
