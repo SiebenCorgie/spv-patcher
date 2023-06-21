9ee3222fa87a15bcdbde9c91bd36a89214919f61
Tendsin Mende <tendsin@protonmail.com>
2023-06-21 16:55:33 +0200

    create *some* spirv-link based constant patcher

--- crates/patch-function/src/constant_replace.rs
+++ crates/patch-function/src/constant_replace.rs
@@ -1,9 +1,17 @@
+use std::io::{BufWriter, Write};
+
+use ahash::AHashMap;
 use spv_patcher::{
     patch::Patch,
-    rspirv::{binary::Assemble, dr::Module},
+    rspirv::{
+        binary::Assemble,
+        dr::{Instruction, Module, Operand},
+        spirv::{Capability, Decoration, LinkageType, Op},
+    },
+    spirv_ext::SpirvExt,
 };
 
-use crate::{function_finder::FuncSignature, FuncIdent};
+use crate::function_finder::FuncSignature;
 
 use thiserror::Error;
 
@@ -25,9 +33,12 @@ pub struct ConstantReplace {
     pub replacement_module: Module,
     ///index into the replacement module's function vector which selects the function that is being replaced.
     pub replacement_index: usize,
+
+    ///If true, the temporary, linkable shader files are kept after linking
+    pub keep_temporary_files: bool,
+
     ///function ident of the function that is replaced in the `replacement_module` context.
-    #[allow(dead_code)]
-    ident: FuncIdent,
+    ident: FuncSignature,
 }
 
 impl ConstantReplace {
@@ -47,27 +58,190 @@ impl ConstantReplace {
                 .map(|arg| arg.result_type.unwrap())
                 .collect();
 
-            FuncIdent::Signature(FuncSignature {
+            FuncSignature {
                 return_type,
                 argument_types,
-            })
+            }
         };
 
         Ok(ConstantReplace {
             replacement_index,
             replacement_module,
+            keep_temporary_files: false,
             ident,
         })
     }
 
     //Merge
-    pub fn merge_modules(
-        &self,
-        _lowered: &spv_patcher::spirt::Module,
-        _dst: &mut spv_patcher::spirt::Module,
-    ) {
-        //we merge the modul by first appending our `lowered` module into `dst`, including the export notation.
-        // After that we
+    pub fn allow_linking(&self, dst: &mut Module) {
+        //Pass1: find exported function name
+        let name = if let Some(f) = self
+            .replacement_module
+            .functions
+            .get(self.replacement_index)
+        {
+            assert!(
+                f.def.as_ref().unwrap().class.opcode == Op::Function,
+                "Def was not a function"
+            );
+
+            let mut is_marked_link = false;
+            let func_id = f.def.as_ref().unwrap().result_id.unwrap();
+            //find decorations for function id, one of which must contain LinkageAttributes
+
+            let mut linkage_name = String::with_capacity(0);
+            'linkage_search_loop: for dec in self.replacement_module.annotations.iter() {
+                log::info!("dec: {:#?}", dec);
+
+                match (dec.operands.get(0), dec.operands.get(1)) {
+                    (
+                        Some(Operand::IdRef(r)),
+                        Some(Operand::Decoration(Decoration::LinkageAttributes)),
+                    ) => {
+                        if *r == func_id {
+                            log::info!("Found linkage attributes for function: {}", func_id);
+
+                            for operand in &dec.operands[2..] {
+                                match operand {
+                                    Operand::LiteralString(s) => linkage_name = s.clone(),
+                                    Operand::LinkageType(LinkageType::Export) => {
+                                        is_marked_link = true
+                                    }
+                                    _ => {}
+                                }
+                            }
+                            break 'linkage_search_loop;
+                        }
+                    }
+                    _ => {}
+                }
+            }
+
+            if !is_marked_link {
+                log::warn!("While we found a linkage function (with name {}), it is not marked *export*. Continuing ...", linkage_name);
+            }
+            log::info!("Found linkage function: {}", linkage_name);
+            linkage_name
+        } else {
+            log::error!(
+                "Could not find {}-th function in replacement module. Linking will fail!",
+                self.replacement_index
+            );
+            return;
+        };
+
+        //Pass2: find a function with the same-ish signature, and add the linkage decoration needed for the linker to
+        //       find it.
+        //NOTE: We currently just test for a similar-ish signature by building both type trees and then
+        //      comparing the parameter types of both.
+        //
+        //      This is currently not *perfect*. For that we'd merge the patch function into the `dst` module
+        //      before linking. That's what the spirv-linker does. However, in our case the target function is not
+        //      decorated with linkage attributes which makes that harder.
+        //
+        //      In the future there might be a SPIR-T based merge pass that would allow that.
+
+        let mut src_arg_ty_table = self
+            .ident
+            .argument_types
+            .iter()
+            .map(|i| (*i, None))
+            .collect::<AHashMap<u32, Option<u32>>>();
+
+        'function_def_iter: for f in dst.functions.iter() {
+            if f.parameters.len() == self.ident.argument_types.len() {
+                //we do that by pattern matching for now 👀
+                // check if type pattern is the same.
+                for (dst_f_ty, ident_ty) in
+                    f.parameters.iter().zip(self.ident.argument_types.iter())
+                {
+                    let dst_f_ty = *dst_f_ty.result_type.as_ref().unwrap();
+                    if let Some(seen_ty) = src_arg_ty_table.get_mut(ident_ty) {
+                        //don't know the mapping atm. push into table
+                        if let Some(seen_dst_ty) = seen_ty {
+                            if dst_f_ty != *seen_dst_ty {
+                                log::error!("We know type={} in the patch module as type={} in the destination module already. However, we tried mapping as type={}.", ident_ty, seen_dst_ty, dst_f_ty);
+                                continue 'function_def_iter;
+                            }
+                        } else {
+                            //need to insert seen type
+                            *seen_ty = Some(dst_f_ty);
+                        }
+                    } else {
+                        log::error!("src_arg_ty_table was incomplete, Linking will fail!");
+                        return;
+                    }
+                }
+
+                //if no continue was called up to this point, decorate this function with the correct call
+                log::info!("Found matching function in dst");
+
+                if !dst.has_extension("Linkage") {
+                    //if needed, add linkage attrib
+                    dst.extensions.push(Instruction::new(
+                        Op::Capability,
+                        None,
+                        None,
+                        vec![Operand::Capability(Capability::Linkage)],
+                    ));
+                }
+                //therefor insert decorations into annotations
+                dst.annotations.push(Instruction::new(
+                    Op::Decorate,
+                    f.def_id(),
+                    None,
+                    vec![
+                        Operand::Decoration(Decoration::LinkageAttributes),
+                        Operand::LiteralString(name.clone()),
+                        Operand::LinkageType(LinkageType::Import),
+                    ],
+                ));
+            }
+        }
+    }
+
+    fn call_linker(&self, dst: &mut Module) -> Result<(), ConstantReplaceError> {
+        const LINK_DST_FILENAME: &'static str = "dst_linkable.spv";
+        const LINK_PATCH_FILENAME: &'static str = "patch_linkable.spv";
+
+        let assembled_dst = dst.assemble();
+        let assembled_patch = self.replacement_module.assemble();
+
+        //write both to a file, sadly, the linker can not yet read from stdin.
+        let dst_linkable_file = std::fs::OpenOptions::new()
+            .create(true)
+            .append(false)
+            .write(true)
+            .open(LINK_DST_FILENAME)?;
+        let mut dst_writer = BufWriter::new(dst_linkable_file);
+        dst_writer.write(bytemuck::cast_slice(&assembled_dst))?;
+
+        let patch_linkable_file = std::fs::OpenOptions::new()
+            .create(true)
+            .append(false)
+            .write(true)
+            .open(LINK_PATCH_FILENAME)?;
+        let mut patch_writer = BufWriter::new(patch_linkable_file);
+        patch_writer.write(bytemuck::cast_slice(&assembled_patch))?;
+
+        //now try to call the linker
+
+        let out = std::process::Command::new("spirv-link")
+            .args([
+                "--verify-ids",
+                "-o",
+                "patched.spv",
+                LINK_DST_FILENAME,
+                LINK_PATCH_FILENAME,
+            ])
+            .output();
+
+        match out {
+            Ok(r) => log::info!("Successfully linked files: {:?}!", r),
+            Err(e) => log::error!("Failed to link files!\n{}", e),
+        }
+
+        Ok(())
     }
 }
 
@@ -76,38 +250,17 @@ impl Patch for ConstantReplace {
         self,
         mut patcher: spv_patcher::patch::Patcher<'a>,
     ) -> Result<spv_patcher::patch::Patcher<'a>, spv_patcher::PatcherError> {
-        let (spirt, spttcx) = patcher.ir_state.as_spirt();
+        let spv = patcher.ir_state.as_spirv();
 
-        //For constant replace we need to merge the patcher's *context* and the
-        // context of the module we are merging.
+        //Right now we do the following:
         //
-        // In SPIRV this would lead to messy id rewriting etc. However, in SPIRT,
-        // we can just lower both modules into the same context.
-        // Then we copy over one module to the other and rewire the function calls.
+        // 1. Rewrite `spv` to allow re-linking the target function.
+        // 2. Rewrite the patching function in order to be used by the spirv-linker
+        // 3. Execute the spirv-linker binary which should produce the merged/linked binary
+        self.allow_linking(spv);
 
-        //lower `replacement_module` into spir
-        let to_lower_spv = self.replacement_module.assemble();
-        let printer = spv_patcher::DisassamblerPrinter::from_words(&to_lower_spv);
-        log::info!("SPVPATCH:\n{}", printer);
-
-        let lowered_replacment = spv_patcher::spirt::Module::lower_from_spv_bytes(
-            spttcx.clone(),
-            bytemuck::cast_slice(&to_lower_spv).to_vec(),
-        )?;
-
-        eprintln!(
-            "\nREPLACEMENT:\n{}\n",
-            spv_patcher::spirt::print::Plan::for_module(&lowered_replacment).pretty_print()
-        );
-
-        eprintln!(
-            "\nDST:\n{}\n",
-            spv_patcher::spirt::print::Plan::for_module(&spirt).pretty_print()
-        );
-
-        //TODO find function in `lowered` and write it to
-        //     `spirt`, then rewrite function calls in `spirt` to use new
-        //     function.
+        self.call_linker(spv)
+            .map_err(|e| spv_patcher::PatcherError::Internal(e.into()))?;
 
         Ok(patcher)
     }
